81141d9cc34c8230e8181e52e7e7d9e44f28a376
lmislm <lmjustlm@163.com>
2017-04-04 21:51:00 +0800

    连接index输入内容，加input的content，添加form表单，

--- js/base.js
+++ js/base.js
@@ -4,5 +4,17 @@
 ;(function () {
     'use strict';
 
-    //alert(1);
+    var $form_add_task = $('.add-task')
+        , new_task = {}
+        ;
+
+    $form_add_task.on('submit',function (e) {
+        // 禁用默认行为
+        e.preventDefault();
+        // 获取新task的值
+        new_task.content = $(this).find('input[name=content]').val();
+       console.log('new_task',new_task);
+
+    })
+
 })();
