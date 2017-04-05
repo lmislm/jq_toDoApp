948216a989618bf505fdc908091a927f03b5400f
lmislm <lmjustlm@163.com>
2017-04-05 21:02:36 +0800

    弹出日期层

--- js/base.js
+++ js/base.js
@@ -5,14 +5,23 @@
     'use strict';
 
     var $form_add_task = $('.add-task')
-        , $delete_task
+        // 注册
+        , $task_delete_trigger
+        , $task_detail
         , new_task = {}
-        , task_list = {}
+        // , task_list = {}
+        , task_list = []
+        , $task_detail = $('.task-detail')
+        , $task_detail_mask = $('.task-detail-mask')
+        , $task_detail_trigger
         ;
 
         init();
 
+
     $form_add_task.on('submit',on_add_task_form_submit)
+    $task_detail_mask.on('click',hide_task_detail)
+
     function on_add_task_form_submit(e) {
         var new_task = {}, $input;
         // 禁用默认行为
@@ -31,14 +40,58 @@
         // console.log('new_task.content',new_task.content);
     }
 
+    function listen_task_detail() {
+        console.log('$task_delete_trigger',$task_delete_trigger);
+        $task_detail_trigger.on('click',function () {
+            var $this = $(this)
+            var $item = $this.parent().parent();
+            var index = $item.data('index');
+            show_task_detail(index);
+            // console.log('index',index);
+        })
+    }
+
+    function show_task_detail(index) {
+        render_task_detail(index);
+        $task_detail.show();
+        $task_detail_mask.show();
+    }
+    // 渲染指定详细信息
+    function render_task_detail(index) {
+        if(index === undefined || !task_list[index])
+            return;
+        var item = task_list[index];
+        console.log('item',item);
+        var tpl ='<div>' +
+        '<div class="content">' +
+           item.content +
+           '</div>' +
+           '<div>' +
+           '<div class="desc">' +
+           '<textarea value="' + item.desc + '"></textarea>' +
+           '</div>' +
+           '</div>' +
+           '<div class="remind">' +
+           '<input type="date"  >' +
+           '</div>' +
+           '</div>';
+        $task_detail.html(null)
+        $task_detail.html(tpl)
+    }
+    function hide_task_detail() {
+        $task_detail.hide();
+        $task_detail_mask.hide();
+    }
+
     function listen_task_delete() {
         // 添加事件之后，要监听删除特定元素
-        $delete_task.on('click',function () {
+
+        $task_delete_trigger.on('click',function () {
             var $this = $(this);
             var $item = $this.parent().parent();
             var index = $item.data('index');
             var tmp = confirm('确定删除');
-            tmp ? delete_task(index) : null;
+            tmp ? task_delete(index) : null;
             // console.log('$item_data(index)', $item.data('index'));
             // 之后需要更新文档流
         })
@@ -53,27 +106,27 @@
          refresh_task_list();
          return true;
      }
-     
+
      // 删除之后刷新loalStorage数据并渲染模板item
     function refresh_task_list() {
         store.set('task_list',task_list);
         render_task_list();
     }
     // 删除一条Task
-     function delete_task(index) {
+     function task_delete(index) {
         // 如果没index，或不存在，则直接返回
          if(index === undefined || !task_list[index]) return;
          delete task_list[index];
          // 更新
          refresh_task_list();
      }
-     
+
     function init() {
         task_list = store.get('task_list')  || [];
         if(task_list.length)
             render_task_list();
     }
-    
+
     function render_task_list() {
         // console.log('1',1);
         var $task_list = $('.task-list');
@@ -83,9 +136,10 @@
             var $task = render_task_item(task_list[i], i);
             $task_list.append($task);
         }
-        $delete_task  = $('.action.delete')
+        $task_delete_trigger  = $('.action.delete')
+        $task_detail_trigger = $('.action.detail')
         listen_task_delete();
-
+        listen_task_detail();
     }
     function render_task_item(data, index) {
         if(!data || !index) return;
@@ -95,7 +149,7 @@
             '<span class="task-content">'+ data.content +'</span>' +
                 '<span class="floatR">' +
                     '<span class="action delete"> 删除 </span>' +
-                    '<span class="action"> 详细 </span>' +
+                    '<span class="action detail"> 详细 </span>' +
             '   </span>' +
             '</div>';
         return $(list_item_tpl)
